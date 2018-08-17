1bcd194d33d5f6822966f9c11f528d7906546a2c
ItaloBoss <italoboss@gmail.com>
2018-08-17 13:57:41 -0300

    First commit

---
+++ PostsApp/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  PostsApp
+//
+//  Created by Ada 2018 on 17/08/2018.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+import Foundation
+
+enum Config {
+    static let baseURL = NSURL(string: "https://jsonplaceholder.typicode.com/")!
+}
